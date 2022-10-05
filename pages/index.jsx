0746770986b9ec5cafcbf79a56bd8f61a47dcb1a
Vedanta Somnathe <vedanta1412@gmail.com>
2022-10-05 09:53:06 +0530

    index + upload

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,61 @@
+import Image from "next/image";
+import style from "../styles/landing.module.css";
+import Link from "next/link";
+
 const Home = () => {
-    return <></>;
+    return (
+        <main className="main">
+            <section className={`cont ${style.landing}`}>
+                <nav className={style.nav}>
+                    <Image
+                        alt="logo"
+                        width="150"
+                        height="30"
+                        src="/logo@2x.png"
+                    />
+                    <Link href="/auth">
+                        <a className={`${style.btn}`}>Login</a>
+                    </Link>
+                </nav>
+
+                <h1 className={style.title}>
+                    Enhance your beauty
+                    <br />
+                    and Skin-care
+                    <br />
+                    Experience
+                </h1>
+                <p className={style.p}>
+                    Pictures are used by the Skinesta app to evaluate your skin
+                    type and provide you with the best possible schedule and
+                    products
+                </p>
+                <div className={style.btns}>
+                    <Link href="/">
+                        <a
+                            className={`${style.btn}`}
+                            style={{ marginRight: "14px" }}
+                        >
+                            Explore
+                        </a>
+                    </Link>
+                    <Link href="/">
+                        <a className={`${style.btn} ${style.btnSecondary}`}>
+                            Store
+                        </a>
+                    </Link>
+                </div>
+            </section>
+            <div className={style.img}>
+                <Image
+                    src="/img@2x.png"
+                    alt="img"
+                    width="500"
+                    height="1000"
+                    objectFit="cover"
+                />
+            </div>
+        </main>
+    );
 };
 export default Home;
---
+++ pages/upload.jsx
@@ -0,0 +1,7 @@
+const upload = () => {
+    // https://api.tcscubo.com/ai_computer_vision/orbo/skin_analysis/v1/media/skinanalysis/upload
+
+    return <main className="main"></main>;
+};
+
+export default upload;

d34e3aa0e151187d5d8c214c5df2aa4dbc4ed08b
Vedanta Somnathe <vedanta1412@gmail.com>
2022-10-06 10:05:52 +0530

    bug fixes

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -3,87 +3,93 @@ import Container from "components/Container";
 import Image from "next/image";
 import Link from "next/link";
 import {
-	ShoppingBag,
-	LogOut,
-	Heart,
-	ShoppingCart,
-	Bell,
-	CreditCard,
-	HelpCircle,
-	User,
+    ShoppingBag,
+    LogOut,
+    Heart,
+    ShoppingCart,
+    Bell,
+    CreditCard,
+    HelpCircle,
+    User,
 } from "react-feather";
 
 const profile = () => {
-	return (
-		<>
-			<Container>
-				<div className={styles.profileBox}>
-					<div className={styles.profileImg}>
-						<Image
-							src={require("@/public/user_face.jpg")}
-							width="80"
-							height="80"
-							style={{ borderRadius: "50%" }}
-						/>
-					</div>
-					<div className={styles.profileContentBox}>
-						<h3 className={styles.profileName}>User Name</h3>
-						<h3 className={styles.profileEmail}>user@mail.com</h3>
-					</div>
-				</div>
+    return (
+        <>
+            <Container>
+                <div className={styles.profileBox}>
+                    <div className={styles.profileImg}>
+                        <Image
+                            src={require("@/public/user_face.jpg")}
+                            width="80"
+                            height="80"
+                            style={{ borderRadius: "50%" }}
+                            alt="alt"
+                        />
+                    </div>
+                    <div className={styles.profileContentBox}>
+                        <h3 className={styles.profileName}>User Name</h3>
+                        <h3 className={styles.profileEmail}>user@mail.com</h3>
+                    </div>
+                </div>
 
-				<ul className={styles.list}>
-					<li>
-						<User className={styles.icon} />
-						<Link href="/profile/editProfile">
-							<a>Edit Profile</a>
-						</Link>
-					</li>
-					<li>
-						<ShoppingBag className={styles.icon} />
-						<Link href="/shop/orders">
-							<a>Orders</a>
-						</Link>
-					</li>
-					<li>
-						<ShoppingCart className={styles.icon} />
-						<Link href="/shop/orders">
-							<a>Cart</a>
-						</Link>
-					</li>
-					<li>
-						<Heart className={styles.icon} />
-						<Link href="/shop/orders">
-							<a>Favourites</a>
-						</Link>
-					</li>
-					<li>
-						<CreditCard className={styles.icon} />
-						<Link href="/shop/orders">
-							<a>Payment Methods</a>
-						</Link>
-					</li>
-					<li>
-						<Bell className={styles.icon} />
-						<Link href="/shop/orders">
-							<a>Notifications</a>
-						</Link>
-					</li>
-					<li>
-						<HelpCircle className={styles.icon} />
-						<Link href="/shop/orders">
-							<a>Help</a>
-						</Link>
-					</li>
-				</ul>
+                <ul className={styles.list}>
+                    <li>
+                        <User className={styles.icon} />
+                        <Link href="/profile/editProfile">
+                            <a>Edit Profile</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <ShoppingBag className={styles.icon} />
+                        <Link href="/shop/orders">
+                            <a>Orders</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <ShoppingCart className={styles.icon} />
+                        <Link href="/shop/orders">
+                            <a>Cart</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Heart className={styles.icon} />
+                        <Link href="/shop/orders">
+                            <a>Favourites</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <CreditCard className={styles.icon} />
+                        <Link href="/shop/orders">
+                            <a>Payment Methods</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Bell className={styles.icon} />
+                        <Link href="/shop/orders">
+                            <a>Notifications</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <HelpCircle className={styles.icon} />
+                        <Link href="/shop/orders">
+                            <a>Help</a>
+                        </Link>
+                    </li>
+                </ul>
 
-				<button className={styles.logoutBtn}>
-					<LogOut className={styles.logoutBtnIcon} color="#5ec396" />
-					<p>Log Out</p>
-				</button>
-			</Container>
-		</>
-	);
+                <Link href="/logout">
+                    <a className={styles.logoutBtn}>
+                        <LogOut
+                            className={styles.logoutBtnIcon}
+                            color="#5ec396"
+                        />
+                        <p>Log Out</p>
+                    </a>
+                </Link>
+            </Container>
+        </>
+    );
 };
 
 export default profile;
